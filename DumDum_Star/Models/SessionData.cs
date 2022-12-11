969312555ef38607e57fa73191794949d4e4d7ee
Locked15 <almgal353@gmail.com>
2022-12-12 04:53:34 +0500

    Basket icon now dynamically change by your basket content.

--- DumDum_Star/Models/SessionData.cs
+++ DumDum_Star/Models/SessionData.cs
@@ -6,12 +6,23 @@ namespace DumDum_Star.Models
     {
         public static Choom? CurrentChoom { get; private set; }
 
+        public static Order? CurrentOrder { get; private set; }
+
         public static string GetHeaderText()
         {
             if (CurrentChoom == null)
                 return "Гость";
 
             return CurrentChoom.Login;
+        }
+
+        public static string GetBasketIconPath()
+        {
+            if (CurrentOrder?.CyberWareToOrders.Any() ?? false)
+                return "./icons/light/header/basket/basket-new.png";
+
+            else
+                return "./icons/light/header/basket/basket.png";
         }
     }
 }
